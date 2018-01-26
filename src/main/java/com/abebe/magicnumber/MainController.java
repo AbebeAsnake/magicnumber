682fbc674e9f510cc8a7007d01c10a3aacf21a29
Abebe Asnake <abexasnex@gmail.com>
2018-01-26 10:46:28 -0500

    Initial commit

---
+++ src/main/java/com/abebe/magicnumber/MainController.java
@@ -0,0 +1,28 @@
+package com.abebe.magicnumber;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class MainController {
+    @RequestMapping("/home")
+    public String showWithName(@RequestParam("number") int number)
+    {
+        String val="";
+       while(number>1){
+            if(number%2==0){
+                number=number/2;
+            }
+            else{
+                number=3*number +1;
+            }
+            val +=Integer.toString(number)+"<br/>";
+        }
+     System.out.println(number);
+        return val;
+    }
+
+}
